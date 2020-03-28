7df5a4c1155e196ca7863f83c2dfd1327b3b577e
strifuno <s.trifunovic@dodax.com>
2020-03-28 16:47:22 +0100

    add app

---
+++ app/src/main/java/at/strifuno/diceroller/MainActivity.kt
@@ -0,0 +1,42 @@
+package at.strifuno.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = getRandomDrawableResource(randomInt)
+
+        val diceImage : ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
+    }
+
+    private fun getRandomDrawableResource(randomInt: Int): Int {
+        return when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+}
